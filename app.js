b9b6bc067db0b32a1dbf2d05ff392cd3f0c8a6ae
AnasAlsallak <anasalsallak@gmail.com>
2023-02-08 03:25:01 +0300

    Modified the app.js code

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+var uname= prompt("Please enter your name:");
+
+while (uname === "") {
+    alert("Invalid input please enter a valid name");
+    uname= prompt("Please enter your name:");
+}
+
+var gender= prompt("Please enter your gender:");
+var f;
+while (gender !== "male" && gender !== "female" || gender === "") {
+    alert("Invalid input please choose male/female");
+    gender= prompt("Please enter your gender:");
+    f=confirm("Do you want to continue with your input?");
+    if(f) {
+        break;
+    }
+}
+
+var age= prompt("Please enter your age:");
+
+while (age <= 0) {
+    alert("Invalid input please enter a value greater than zero");
+    age= prompt("Please enter your age:");
+}
+
+var welcome= confirm("Do you wish to skip the Welcomeing message!?");
+
+if(!welcome){
+switch (gender) {
+ case "male":
+    alert("Welcome Mr "+ uname);
+    break;
+ case "female":
+    alert("Welcome Mrs "+ uname);
+    break;
+ case false:
+    break;
+ default:
+    alert("Welcome "+ uname);
+}
+}
+
+
+
+
+
